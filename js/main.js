bd221aa9c78be23dc7298e08d2ad7753d5cf9bad
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-19 10:44:32 +0545

    Code cleanup !!

--- js/main.js
+++ js/main.js
@@ -14,57 +14,17 @@ import {
 } from "./constants.js";
 
 
-class Ball {
-    /**
-     * Represents a ball, that can be used in canvas.
-     * DEFAULT variables are defined at ./constants.js
-     *
-     * @param {Number} [x]                  X position of the ball, if not provided random value will be chosen.
-     * @param {Number} [y]                  Y position of the ball, if not provided random value will be chosen.
-     * @param {Number} [radius]             Radius of the ball, if not provided random value will be chosen.
-     * @param {String} [color]              Color of the ball, if not provided random value will be chosen.
-     * @param {Number} [speed]              Speed of the ball, if not provided random value will be chosen.
-     * @param {Number} [xDirection=-1|1]    Starting direction of ball in x axis, if not provided random value will be chosen.
-     * @param {Number} [yDirection=-1|1]    Starting direction of ball in y axis, if not provided random value will be chosen.
-     */
-    constructor(x, y, radius, color, speed, xDirection, yDirection) {
-        this.radius = radius || random.randInt(BALL_MIN_RADIUS, BALL_MAX_RADIUS);
-        this.color = color || random.getRandomHexColor();
-        this.speed = speed || random.randInt(BALL_MIN_SPEED, BALL_MAX_SPEED);
+class CollidingObject {
+    constructor(x, y, speed, xDirection, yDirection) {
+        this.startX = 0;
+        this.startY = 0;
+        this.endX = 0;
+        this.endY = 0;
 
-        this.startX = this.radius;
-        this.endX = CANVAS_WIDTH - this.radius;
-        this.startY = this.radius;
-        this.endY = CANVAS_HEIGHT - this.radius;
-
-        this.x = x || random.randInt(this.startX, this.endX);
-        this.y = y || random.randInt(this.startY, this.endY);
+        this.speed = speed;
 
         this.xDirection = xDirection || Math.random() > 0.5 ? -1 : 1;
         this.yDirection = yDirection || Math.random() > 0.5 ? -1 : 1;
-    }
-
-    /**
-     * Changes direction of balls on collision.
-     *
-     * @param {Ball[]} balls      Array
-     * */
-    changeDirectionOnCollision = (balls) => {
-        balls.forEach(ball => {
-            if (ball !== this) {
-                const distanceBetweenBalls = calculateDistanceBetweenBalls(this, ball);
-                const sumOfBallRadius = this.radius + ball.radius;
-                const ballTouchPosition = sumOfBallRadius - distanceBetweenBalls;
-
-                if (sumOfBallRadius >= distanceBetweenBalls) {
-                    this.changeXDirection();
-                    this.changeYDirection();
-
-                    this.x += ballTouchPosition * this.xDirection;
-                    this.y += ballTouchPosition * this.yDirection;
-                }
-            }
-        });
     }
 
     /**
@@ -76,6 +36,13 @@ class Ball {
      * Changes ball's Y direction.
      * */
     changeYDirection = () => this.yDirection = -this.yDirection;
+
+    /**
+     * Changes direction of objects on collision, must accept all objects as parameter
+     * */
+    changeDirectionOnCollision = () => {
+        throw Error("Override `changeDirectionOnCollision` method, and define how the collision is handled");
+    }
 
     /**
      * Updates ball position, using speed and direction && Frees the balls that is stuck at the end of window.
@@ -99,6 +66,67 @@ class Ball {
 
         this.x += this.speed * this.xDirection;
         this.y += this.speed * this.yDirection;
+    }
+
+    /**
+     * Must Accept context as argument
+     * */
+    render = () => {
+        throw Error("Override `render` method, and define how object must render");
+    }
+}
+
+
+class Ball extends CollidingObject {
+    /**
+     * Represents a ball, that can be used in canvas.
+     * DEFAULT variables are defined at ./constants.js
+     *
+     * @param {Number} [x]                  X position of the ball, if not provided random value will be chosen.
+     * @param {Number} [y]                  Y position of the ball, if not provided random value will be chosen.
+     * @param {Number} [radius]             Radius of the ball, if not provided random value will be chosen.
+     * @param {String} [color]              Color of the ball, if not provided random value will be chosen.
+     * @param {Number} [speed]              Speed of the ball, if not provided random value will be chosen.
+     * @param {Number} [xDirection=-1|1]    Starting direction of ball in x axis, if not provided random value will be chosen.
+     * @param {Number} [yDirection=-1|1]    Starting direction of ball in y axis, if not provided random value will be chosen.
+     */
+    constructor(x, y, radius, color, speed, xDirection, yDirection) {
+        super(x, y, speed, xDirection, yDirection);
+
+        this.radius = radius || random.randInt(BALL_MIN_RADIUS, BALL_MAX_RADIUS);
+        this.color = color || random.getRandomHexColor();
+        this.speed = speed || random.randInt(BALL_MIN_SPEED, BALL_MAX_SPEED);
+
+        this.startX = this.radius;
+        this.endX = CANVAS_WIDTH - this.radius;
+        this.startY = this.radius;
+        this.endY = CANVAS_HEIGHT - this.radius;
+
+        this.x = x || random.randInt(this.startX, this.endX);
+        this.y = y || random.randInt(this.startY, this.endY);
+    }
+
+    /**
+     * Changes direction of balls on collision.
+     *
+     * @param {Ball[]} balls      Array
+     * */
+    changeDirectionOnCollision = (balls) => {
+        balls.forEach(ball => {
+            if (ball !== this) {
+                const distanceBetweenBalls = calculateDistanceBetweenBalls(this, ball);
+                const sumOfBallRadius = this.radius + ball.radius;
+                const ballTouchPosition = sumOfBallRadius - distanceBetweenBalls;
+
+                if (sumOfBallRadius >= distanceBetweenBalls) {
+                    this.changeXDirection();
+                    this.changeYDirection();
+
+                    this.x += ballTouchPosition * this.xDirection;
+                    this.y += ballTouchPosition * this.yDirection;
+                }
+            }
+        });
     }
 
     render = (ctx) => {
@@ -126,8 +154,10 @@ class Ball {
  * @param {Number} [yDirection=-1|1]    Starting direction of ant in y axis, if not provided random value will be chosen.
  * @param {String} [image]              Image of the ant, if not provided random image will be chosen.
  */
-class Ant {
+class Ant extends CollidingObject {
     constructor(x, y, speed, width, height, xDirection, yDirection, image) {
+        super(x, y, speed, xDirection, yDirection);
+
         this.speed = speed || random.randInt(ANT_MIN_SPEED, ANT_MAX_SPEED);
 
         this.width = width || random.randInt(ANT_MIN_WIDTH, ANT_MAX_WIDTH);
@@ -143,53 +173,15 @@ class Ant {
         this.y = y || random.randInt(this.startY, this.endY);
 
         this.image = image || ANT_IMAGE;
-
-        this.xDirection = xDirection || Math.random() > 0.5 ? -1 : 1;
-        this.yDirection = yDirection || Math.random() > 0.5 ? -1 : 1;
     }
 
-    updatePosition = () => {
-        if (this.x >= this.endX) {
-            this.changeXDirection();
-            this.x = this.endX;
-        } else if (this.x <= this.startX) {
-            this.changeXDirection();
-            this.x = this.startX;
-        }
-
-        if (this.y >= this.endY) {
-            this.changeYDirection();
-            this.y = this.endY;
-        } else if (this.y <= this.startY) {
-            this.changeYDirection();
-            this.y = this.startY;
-        }
-
-        this.x += this.speed * this.xDirection;
-        this.y += this.speed * this.yDirection;
-    }
-
-    changeDirectionOnCollision = (ants) => {
-
-    }
-
-    /**
-     * Changes ball's X direction.
-     * */
-    changeXDirection = () => this.xDirection = -this.xDirection;
-
-    /**
-     * Changes ball's Y direction.
-     * */
-    changeYDirection = () => this.yDirection = -this.yDirection;
+    changeDirectionOnCollision = () => null;
 
     render = (ctx) => {
         this.updatePosition();
 
         ctx.beginPath();
-
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-
         ctx.closePath();
     }
 }
@@ -251,7 +243,7 @@ class Canvas {
 
         objects.forEach(object => {
             object.render(this.ctx);
-            object.changeDirectionOnCollision(object)
+            object.changeDirectionOnCollision(objects)
         })
 
         requestAnimationFrame(() => this.render(objects));
@@ -281,12 +273,12 @@ function createAndRenderBalls(
     const balls = [];
 
     for (let i = 0; i < numberOfBalls; i++) {
-        let ball = new Ant(null, null, radius, color, speed, xDirection, yDirection);
+        let ball = new Ball(null, null, radius, color, speed, xDirection, yDirection);
 
         if (i !== 0) {
             for (let j = 0; j < balls.length; j++) {
                 if ((balls[j].radius + ball.radius) >= calculateDistanceBetweenBalls(balls[j], ball)) {
-                    ball = new Ant(null, null, radius, color, speed, xDirection, yDirection);
+                    ball = new Ball(null, null, radius, color, speed, xDirection, yDirection);
                     j = -1;
                 }
             }
@@ -299,4 +291,4 @@ function createAndRenderBalls(
 }
 
 
-createAndRenderBalls(50);
+createAndRenderBalls(1000);
